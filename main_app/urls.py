036018d1db0ed94d7dc63a523f02564c7bf79465
hdmcgehee <hdmcgehee@gmail.com>
2020-08-05 13:59:05 -0400

    signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('', views.home, name='home'),
 
 
+    # ___________    SIGNUP      ____________
+    path('', views.home, name='signup'),
+
+
     # ___________    CITY DETAIL    _________
     path('city/', views.city_detail, name = 'city_detail')
 
