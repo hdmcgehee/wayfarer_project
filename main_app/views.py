28bfdb26b315cad6485d45088f080c65bb80adb3
nathanjh-28 <nathan@nathanjharris.com>
2020-08-06 23:12:11 -0600

    cleaned up code and added comment headers for readability

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,24 @@
+
+# ___________       HTTP RESPONSES   ______________________
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+#      ___________       AUTH   ___________________________
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+
+#  ___________     MODELS AND FORMS   _____________________ 
 from .models import Profile, Post, City
 from .forms import ProfileForm, CityForm, PostForm
 
 
-# ___________    Home Route  __________
-@login_required
-def home(request):
-    HttpResponse('home')
+# _________________________________________________________
+# ___________________     USER     ________________________
+# _________________________________________________________
 
 
-# ___________    Sign Up Route  __________
+# ________________   Sign Up Route    _____________________
 def signup(request):
     error = None
     form = UserCreationForm()
@@ -40,57 +45,21 @@ def signup(request):
     else:
         return render(request, 'home.html', context)
 
-
-# ___________    City Detail Route  __________
-@login_required
-def city_detail(request):
-    return render(request, 'city/detail.html')
-
-
-# ___________    City Index Route  __________
-@login_required
-def city_index(request, city_id):
-    cities = City.objects.all()
-    city = City.objects.get(id=city_id)
-    current_user = request.user
-    id = current_user.id
-    profile = Profile.objects.get(user=id)
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            new_post = form.save(commit = False)
-            new_post.city_id = city_id
-            new_post.profile_id = profile.id
-            new_post.save()
-
-        return redirect('city_index', city_id)
-
-    else:
-        form = PostForm()
-
-        context = {
-            'city': city,
-            'form': form,
-            'cities': cities
-        }
-        return render(request, 'city/city.html', context)
-
-
-# ___________    Profile Route  __________
+# ________________    Profile Route  ______________________
 @login_required
 def profile(request):
     current_user = request.user
     id = current_user.id
     profile = Profile.objects.get(user=id)
+    # POST METHOD
     if request.method == 'POST':
         form = ProfileForm(request.POST, instance=profile)
         if form.is_valid():
             profile = form.save()
             return redirect('profile')
-
+    # GET METHOD
     else:
         form = ProfileForm(instance=profile)
-
         context = {
             'profile': profile,
             'form': form
@@ -98,32 +67,84 @@ def profile(request):
 
         return render(request, 'registration/profile.html', context)
 
+# _________________________________________________________
+# ___________________     CITY     ________________________
+# _________________________________________________________
 
-# ___________    Post Detail Route  __________
+# ________________   City Index Route  ____________________
+@login_required
+def city_index(request, city_id):
+    cities = City.objects.all()
+    city = City.objects.get(id=city_id)
+    current_user = request.user
+    id = current_user.id
+    profile = Profile.objects.get(user=id)
+    # POST METHOD
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit = False)
+            new_post.city_id = city_id
+            new_post.profile_id = profile.id
+            new_post.save()
+        return redirect('city_index', city_id)
+    # GET METHOD
+    else:
+        form = PostForm()
+        context = {
+            'city': city,
+            'form': form,
+            'cities': cities
+        }
+        return render(request, 'city/city.html', context)
+
+# __________________________________________________________
+# ___________________     POSTS     ________________________
+# __________________________________________________________
+
+# ________________     Post Detail    _____________________
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     profile = Profile.objects.get(id=post.profile_id)
+    context = {
+        'post': post, 
+        'profile': profile
+    }
+    return render(request, 'post_detail.html', context)
 
-    return render(request, 'post_detail.html', {'post': post, 'profile': profile})
-
-
+# _________________     Post EDIT   _______________________
 def post_edit(request, post_id):
-  post = Post.objects.get(id=post_id)
+    post = Post.objects.get(id=post_id)
+    # POST METHOD
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save()
+            return redirect('city_index', post.city_id)
+    # GET METHOD
+    else:
+        form = PostForm(instance = post)
+        return render(request, 'post_edit.html', {'post': post, 'form': form})
 
-
-  if request.method == 'POST':
-    form = PostForm(request.POST, instance=post)
-    if form.is_valid():
-      new_post = form.save()
-      return redirect('city_index', post.city_id)
-
-  else:
-    form = PostForm(instance = post)
-    return render(request, 'post_edit.html', {'post': post, 'form': form})
-
+# _______________      Post DELETE   ______________________
 def post_delete(request, post_id):
   post = Post.objects.get(id = post_id)
-  city = post.city_id
-
+  city_id = post.city_id
   Post.objects.get(id=post_id).delete()
-  return redirect('city_index', city)
+  return redirect('city_index', city_id)
+
+
+# _________________________________________________________
+# ___________________     TBD     _________________________
+# _________________________________________________________
+
+
+# ________________    Home Route     ______________________ OLD
+@login_required
+def home(request):
+    HttpResponse('home')
+
+# ___________      City Detail Route   _____________________OLD
+@login_required
+def city_detail(request):
+    return render(request, 'city/detail.html')
