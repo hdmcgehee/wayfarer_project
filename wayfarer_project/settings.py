ea9f7ff2feb8df4ee67a3e175a6f4ed5842a5e53
nathanjh-28 <nathan@nathanjharris.com>
2020-08-10 10:59:28 -0600

    trying to fix typo number 2 for heroku

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -10,11 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
+import django_on_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+# trying this for heroku deployment
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -126,3 +132,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/profile'
 
 LOGOUT_REDIRECT_URL = '/'
+
+django_on_heroku.settings(locals())
